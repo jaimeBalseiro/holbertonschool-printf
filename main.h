d0214d693baf66359c99859ede5ebc0c610b455e
npadovani <normant_padovani@hotmail.com>
2022-07-12 11:25:56 +0000

    Created the main.h header file.

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+/*From here on below, the prototypes*/
+
+Int _printf(const char *format, …);
+int _strcmp(char *s1, char *s2);
+
+#endif
+
